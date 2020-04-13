35d78d38aa660ba0274b55491f887cc5235facf7
Robb Fountain <robb@131studios.com>
2020-04-13 16:43:43 -0400

    Update MigrationTest.php

--- tests/MigrationTest.php
+++ tests/MigrationTest.php
@@ -3,17 +3,15 @@
 namespace OneThirtyOne\S3Migration\Tests;
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Storage;
 use OneThirtyOne\S3Migration\Facades\FileCollector;
 
 class MigrationTest extends TestCase
 {
     /** @test */
-    public function It_gets_files_from_local_storage()
+    public function It_retrieves_files_from_local_storage()
     {
-        $this->withExceptionHandling();
-
         $this->assertInstanceOf(Collection::class, FileCollector::fromLocalStorage());
+
         $this->assertEquals(3, FileCollector::fromLocalStorage()->count());
     }
 }
