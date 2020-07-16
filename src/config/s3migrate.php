57bfbbe5bf6e6073e5766101e37a05c57a581c08
Robb Fountain <robb@131studios.com>
2020-07-16 12:16:28 -0400

    added storage path config variable

--- src/config/s3migrate.php
+++ src/config/s3migrate.php
@@ -16,4 +16,12 @@ return [
      * This is the AWS S3 bucket where the local files will be saved to
      */
     'aws_bucket' => 'your-bucket-name',
+
+    /**
+     * Storage Path
+     *
+     * This is the default storage path to look for files.  You should not have to change this from
+     * storage_path('app/') but feel free to modify it for your environment.
+     */
+    'storage_path' => storage_path('app/')
 ];
