47f7536c8d3341f1e8054f6c30222b34ba2047c0
Robb Fountain <robb@131studios.com>
2021-05-27 11:23:18 -0400

    refactor

--- src/Migrator.php
+++ src/Migrator.php
@@ -34,7 +34,7 @@ class Migrator
         $this->file = $file ?? $this->file;
 
         $path = Storage::disk('s3')
-            ->putFileAs($this->getPath(), $this->file, $this->file->getFilename());
+            ->putFileAs($this->getFullPath(), $this->file, $this->file->getFilename());
 
         return $path;
     }
@@ -47,15 +47,14 @@ class Migrator
         config(['filesystems.disks.s3.bucket' => config('s3migrate.aws_bucket')]);
     }
 
-    public function getPath()
+    /**
+     * @return \Illuminate\Support\Stringable
+     */
+    public function getFullPath()
     {
-        return Str::of(config('s3migrate.aws_bucket_path'))->endsWith('/')
-            ? Str::of(config('s3migrate.aws_bucket_path'))
-                ->append($this->file->getRelativePath())
-                ->append('/')
-            : Str::of(config('s3migrate.aws_bucket_path'))
-                ->append('/')
-                ->append($this->file->getRelativePath())
-                ->append('/');
+        return Str::of(config('s3migrate.aws_bucket_path'))
+            ->finish('/')
+            ->append($this->file->getRelativePath())
+            ->finish('/');
     }
 }
